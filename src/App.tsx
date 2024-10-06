ecd46f45149beb87ca1c462c24926665d36182d2
supriadiacep <supriadi.acep@gmail.com>
2024-10-06 21:19:18 +0700

    Finish Assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import styles from './App.module.scss';
 
+import { Dialog } from '@/components/Dialog';
+
 function App() {
   return (
     <div className={styles.container}>
       {/* Test your component here */}
       {/* Ensure it matches the design exactly */}
+      <Dialog
+        variant='success'
+        title='Successfully loaded'
+        description='Neque porro quisquam est qui dolorem ipsum'
+      />
+      {/* <Dialog
+        variant='info'
+        title='New information'
+        description='Neque porro quisquam est qui dolorem ipsum'
+        confirmValue='Create'
+        isDisabled={true}
+      /> */}
+      {/* <Dialog
+        variant='danger'
+        title='Are you sure you want to delete this user?'
+        description='This action is irreversible'
+        confirmValue='Delete'
+      /> */}
     </div>
   );
 }
